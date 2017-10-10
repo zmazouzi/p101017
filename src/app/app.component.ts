d0893eff1649d9e031457bd89c4692e1a9526ed4
zmazouzi <zakaryae15@gmail.com>
2017-10-10 22:36:15 +0100

    fuck service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import {AngularFireDatabase, FirebaseListObservable} from "angularfire2/database-deprecated";
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items: FirebaseListObservable<any[]>;
+  constructor(db: AngularFireDatabase) {
+    this.items = db.list('/items');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularFireModule } from 'angularfire2';
+import {AngularFireAuthModule} from "angularfire2/auth";
+import {AngularFireDatabaseModule} from "angularfire2/database";
+import {AngularFireDatabase} from "angularfire2/database-deprecated";
+
+const config = {
+  apiKey: "AIzaSyAo01e9qmplGAtCZRKyiNjLC6V__LOWRjg",
+  authDomain: "ionic-70041.firebaseapp.com",
+  databaseURL: "https://ionic-70041.firebaseio.com",
+  projectId: "ionic-70041",
+  storageBucket: "ionic-70041.appspot.com",
+  messagingSenderId: "644421579632"
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(config, 'my-app-name'),
+    AngularFireAuthModule,
+    AngularFireDatabaseModule
   ],
-  providers: [],
+  providers: [
+    AngularFireDatabase
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
